a8ee82efc2f1339388eaa5d8af75092471920f8c
abi-appusamy9932 <181798726+abi-appusamy9932@users.noreply.github.com>
2025-06-25 16:24:04 +0000

    Auto-format code

--- src/main/java/org/team2342/lib/sensors/distance/DistanceSensorIOSim.java
+++ src/main/java/org/team2342/lib/sensors/distance/DistanceSensorIOSim.java
@@ -2,7 +2,6 @@ package org.team2342.lib.sensors.distance;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 /*
  * Implementing DistanceSensorIO for sim
  */
@@ -12,9 +11,10 @@ public class DistanceSensorIOSim implements DistanceSensorIO {
   private final String entryKey;
   private final double defaultValue;
   /**
-   * Constructor to configure the sim 
+   * Constructor to configure the sim
+   *
    * @param entryKey Unique key to identify this entry
-   * @param defaultValue Just like the name suggests 
+   * @param defaultValue Just like the name suggests
    */
   public DistanceSensorIOSim(String entryKey, double defaultValue) {
     this.entryKey = entryKey;
@@ -27,6 +27,7 @@ public class DistanceSensorIOSim implements DistanceSensorIO {
 
   /**
    * Gets called to update sensor reading
+   *
    * @param inputs The object that stores sensor data
    */
   @Override
