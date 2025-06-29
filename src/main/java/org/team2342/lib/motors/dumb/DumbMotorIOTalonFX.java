b617d55a603e3e2185f49ee51235796757fa744d
abi-appusamy9932 <abinayya.appusamy@gmail.com>
2025-06-29 16:43:42 -0400

    creating DumbMotorIO + talon code

--- src/main/java/org/team2342/lib/motors/dumb/DumbMotorIOTalonFX.java
+++ src/main/java/org/team2342/lib/motors/dumb/DumbMotorIOTalonFX.java
@@ -6,4 +6,62 @@
 
 package org.team2342.lib.motors.dumb;
 
-public class DumbMotorIOTalonFX implements DumbMotorIO {}
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+
+public class DumbMotorIOTalonFX implements DumbMotorIO {
+    private final TalonFX talon;
+
+    private final StatusSignal<Voltage> appliedVolts; 
+    private final StatusSignal<Current> current;
+
+    private final VoltageOut voltageRequest = new VoltageOut(0);
+    private final Debouncer connectedDebouncer = new Debouncer(0.5);
+
+    public DumbMotorIOTalonFX(int canID, DumbMotorConfig config) {
+        talon = new TalonFX(canID);
+
+        TalonFXConfiguration talonConfig = new TalonFXConfiguration();
+        talonConfig.CurrentLimits.SupplyCurrentLimit = config.supplyLimit;
+        talonConfig.CurrentLimits.SupplyCurrentLimitEnable = config.supplyLimit > 0;
+        talonConfig.CurrentLimits.StatorCurrentLimit = config.statorLimit;
+        talonConfig.CurrentLimits.StatorCurrentLimitEnable = config.statorLimit > 0;
+
+        talonConfig.MotorOutput.Inverted = 
+            config.motorInverted ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+
+        talonConfig.MotorOutput.NeutralMode = 
+            config.idleMode == DumbMotorConfig.IdleMode.BRAKE ? NeutralModeValue.Brake : NeutralModeValue.Coast;
+        
+        talon.getConfigurator().apply(talonConfig);
+
+        appliedVolts = talon.getMotorVoltage();
+        current = talon.getSupplyCurrent();
+
+        BaseStatusSignal.setUpdateFrequencyForAll(50.0, appliedVolts, current);
+        talon.optimizeBusUtilization();
+    }
+
+    @Override
+    public void updateInputs(DumbMotorIOInputs inputs) {
+        BaseStatusSignal.refreshAll(appliedVolts, current); 
+        inputs.connected = connectedDebouncer.calculate(
+            BaseStatusSignal.isAllGood(appliedVolts, current));
+        inputs.appliedVolts = appliedVolts.getValueAsDouble();
+        inputs.currentAmps = current.getValueAsDouble();
+    }
+
+    @Override
+    public void runVoltage(double voltage){
+        talon.setControl(voltageRequest.withOutput(voltage));
+    }
+}
