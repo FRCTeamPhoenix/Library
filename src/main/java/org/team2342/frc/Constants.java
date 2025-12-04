63449f91a01db8f39ce51400fe27873003a28fb7
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2025-12-04 09:11:44 -0500

    remove reefscape reference

--- src/main/java/org/team2342/frc/Constants.java
+++ src/main/java/org/team2342/frc/Constants.java
@@ -6,13 +6,10 @@
 
 package org.team2342.frc;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import org.team2342.lib.util.AllianceUtils;
 
 public final class Constants {
   public static final Mode CURRENT_MODE = Mode.SIM;
@@ -48,12 +45,6 @@ public final class Constants {
                 Units.inchesToMeters(-8.9822),
                 Units.inchesToMeters(12.125)),
             new Rotation3d(0, 0.0, Units.degreesToRadians(35)));
-
-    // The layout of the AprilTags on the field
-    static {
-      AllianceUtils.setFieldLayout(
-          AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark));
-    }
 
     // Basic filtering thresholds
     public static final double MAX_AMBIGUITY = 0.1;
